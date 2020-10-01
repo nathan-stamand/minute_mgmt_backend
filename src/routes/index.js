c99ed62ec7680369c7eca3e2af51b97816a10f8b
nathan-stamand <nathansaint810@gmail.com>
2020-10-01 06:27:45 -0500

    Fixed bugs in which indexRouter was referred to as router, change var to const.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,8 @@
 import express from "express";
 
-var router = express.Router();
+const indexRouter = express.Router();
 
-router.get('/', (req, res, next) =>
+indexRouter.get('/', (req, res, next) =>
   res.status(200).json({ message: 'This GET request works bud!' })
 );
 

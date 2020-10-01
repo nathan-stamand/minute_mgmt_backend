5c0d4931371f67c33206a16a72f42d6979dcb846
nathan-stamand <nathansaint810@gmail.com>
2020-10-01 05:44:03 -0500

    Remove defaults and replaced with API-friendly setup

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-router.route('/')
-  .get(function(req, res) {
-    return res.send('received GET HTTP')
-  })
-  .post(function(req, res) {
-    return res.send('received POST HTTP')
-  })
-  .patch(function(req, res) {
-    return res.send('received PATCH HTTP')
-  })
-  .delete(function(req, res) {
-    return res.send('received DELETE HTTP')
-  });
+router.get('/', function(req, res, next) {
+  return res.status(200).json({ message: 'This GET request works bud!' });
+});
 
 module.exports = router;
-

70f6f9e5bdfb40b466130320ae72c82041fdcb0b
nathan-stamand <nathansaint810@gmail.com>
2020-10-04 13:54:26 -0500

    Remove s_ prefix from subtasks columns

--- src/utils/queries.js
+++ src/utils/queries.js
@@ -8,9 +8,9 @@ CREATE TABLE IF NOT EXISTS tasks (
   );
   CREATE TABLE IF NOT EXISTS subtasks (
     id SERIAL PRIMARY KEY,
-    s_task VARCHAR(255) NOT NULL,
-    s_time INT DEFAULT '0',
-    s_complete BOOL NOT NULL DEFAULT '0',
+    task VARCHAR(255) NOT NULL,
+    time INT DEFAULT '0',
+    complete BOOL NOT NULL DEFAULT '0',
     task_id INT,
     FOREIGN KEY (task_id) REFERENCES tasks(id)
     );
@@ -20,7 +20,7 @@ export const insertTasksSubtasks = `
 INSERT INTO tasks(id, task, time, complete)
 VALUES ('1', 'Hygiene Routine', '15', '0'),
       ('2', 'Exercise Routine', '30', '1');
-INSERT INTO subtasks(id, s_task, s_time, s_complete, task_id)
+INSERT INTO subtasks(id, task, time, complete, task_id)
 VALUES ('1', 'Brush teeth', '1', '0', '1'),
         ('2', 'Wash face', '1', '1', '1'),
         ('3', 'Shower', '10', '0', '1')

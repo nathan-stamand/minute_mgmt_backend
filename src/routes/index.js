9439c92577c774130f7d2894c98cf1768f84261e
nathan-stamand <nathansaint810@gmail.com>
2020-10-04 12:43:36 -0500

    Add tasks create (post) and delete routes, create subtasks create route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,19 @@
 import express from 'express';
-import { indexPage, subTasksPage, tasksPage } from '../controllers';
+import {
+  addSubTask,
+  addTask,
+  indexPage,
+  subTasksPage,
+  tasksPage,
+  deleteTask
+} from '../controllers';
 
 const indexRouter = express.Router();
 indexRouter.get('/', indexPage);
 indexRouter.get('/tasks', tasksPage);
+indexRouter.post('/tasks', addTask);
+indexRouter.delete('/tasks/:id', deleteTask);
 indexRouter.get('/subtasks', subTasksPage);
+indexRouter.post('/subtasks', addSubTask);
 
 export default indexRouter;

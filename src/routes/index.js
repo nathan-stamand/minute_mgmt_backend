2e06c9f17d392db04fe57ce223954c2cbc929bb1
nathan-stamand <nathansaint810@gmail.com>
2020-10-04 12:56:29 -0500

    Add delete subtask route

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,15 +5,19 @@ import {
   indexPage,
   subTasksPage,
   tasksPage,
-  deleteTask
+  deleteTask,
+  deleteSubTask
 } from '../controllers';
 
 const indexRouter = express.Router();
 indexRouter.get('/', indexPage);
+
 indexRouter.get('/tasks', tasksPage);
 indexRouter.post('/tasks', addTask);
 indexRouter.delete('/tasks/:id', deleteTask);
+
 indexRouter.get('/subtasks', subTasksPage);
 indexRouter.post('/subtasks', addSubTask);
+indexRouter.delete('/subtasks/:id', deleteSubTask);
 
 export default indexRouter;
